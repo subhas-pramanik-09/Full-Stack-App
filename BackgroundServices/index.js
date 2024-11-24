0602d64682fa83b643475db1fcd8afc64a3799b1
subhas-pramanik-09 <subhaspramanik864@gmail.com>
2024-11-25 00:11:32 +0530

    completed email services

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -3,13 +3,20 @@ const app = express();
 const cron = require("node-cron");
 const dotenv = require("dotenv");
 const dbConnection = require("./utils/db.js");
+const { sendDeatilsProspectEmail } = require("./EmailServices/sendDetailsProspect.js");
+const { sendEligibilityEmail } = require("./EmailServices/sendEligibilityEmail.js");
+const { sendBloodDonationReminder } = require("./EmailServices/sendBloodDonationReminder.js");
+const { sendDonorDetailsEmail } = require("./EmailServices/sendDonorDetailsEmail.js");
 dotenv.config();
 
 // scedule task
 
 const run = () => {
   cron.schedule("* * * * * *", () => {
-    console.log("running a task every second");
+      sendDeatilsProspectEmail();
+      sendEligibilityEmail();
+      sendBloodDonationReminder();
+      sendDonorDetailsEmail();
   });
 };
 
